080253013542a4a6b4c02292d97c0f7c4e1b2750
BuddhaNag12 <rahulnag514@gmail.com>
2021-03-13 10:49:28 +0530

    gsap vue first commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
+
   {
     path: '/',
     name: 'Home',
@@ -13,13 +14,28 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/contact.vue')
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition: any) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,129 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="contact">
+    <h1>Home</h1>
+   <section id="first">
+      <transition-group
+      appear
+      tag="ul"
+      @before-enter="beforeEnter"
+      @enter="enter"
+    >
+      <li v-for="(icon, index) in icons" :key="icon.name" :data-index="index">
+        <span class="material-icons">{{ icon.name }}</span>
+        <div>{{ icon.text }}</div>
+      </li>
+    </transition-group>
+   </section>
+   <section id="second">
+      <transition-group
+      appear
+      tag="ul"
+      @before-enter="beforeEnter"
+      @enter="enter"
+    >
+      <li v-for="(icon, index) in icons" :key="icon.name" :data-index="index">
+        <span class="material-icons">{{ icon.name }}</span>
+        <div>{{ icon.text }}</div>
+      </li>
+    </transition-group>
+   </section>
+   <section id="third">
+      <transition-group
+      appear
+      tag="ul"
+      @before-enter="beforeEnter"
+      @enter="enter"
+    >
+      <li v-for="(icon, index) in icons" :key="icon.name" :data-index="index">
+        <span class="material-icons">{{ icon.name }}</span>
+        <div>{{ icon.text }}</div>
+      </li>
+    </transition-group>
+   </section>
+   <section id="fourth">
+      <transition-group
+      appear
+      tag="ul"
+      @before-enter="beforeEnter"
+      @enter="enter"
+    >
+      <li v-for="(icon, index) in icons" :key="icon.name" :data-index="index">
+        <span class="material-icons">{{ icon.name }}</span>
+        <div>{{ icon.text }}</div>
+      </li>
+    </transition-group>
+   </section>
+   
+    <button @click="ScrollToTop">ScrollToTop</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { computed, defineComponent, ref } from "vue";
+import {ScrollToPlugin} from 'gsap/ScrollToPlugin';
+import gsap from "gsap";
+gsap.registerPlugin(ScrollToPlugin);
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
+  name: "Home",
+
+  setup() {
+    const icons = ref([
+      { name: "alternate_email", text: "by email" },
+      { name: "local_phone", text: "by phone" },
+      { name: "local_post_office", text: "by post" },
+      { name: "local_fire_department", text: "by smoke signal" },
+    ]);
+    const beforeEnter = (el: any) => {
+      el.style.opacity = 0;
+      el.style.transform = "translateY(100px)";
+    };
+    const enter = (el: any, done: any) => {
+      gsap.to(el, {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        onComplete: done,
+        delay: el.dataset.index * 0.2,
+      });
+    };
+    // const afterEnter = (el: Element) => {
+    //   gsap.to(el, {
+    //     opacity: 0,
+    //     y: -100,
+    //     duration: 0.8,
+    //     delay: 1,
+    //   });
+    // };
+    const ScrollToTop = () => {
+      gsap.to(window,{scrollTo:0,duration:1,ease:'ease'})
+    };
+
+    return { icons, beforeEnter, enter,ScrollToTop };
   },
 });
 </script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+
+.contact ul {
+  padding: 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  max-width: 400px;
+  margin: 60px auto;
+}
+.contact li {
+  list-style-type: none;
+  background: white;
+  padding: 30px;
+  border-radius: 10px;
+  box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  line-height: 1.5em;
+}
+</style>
